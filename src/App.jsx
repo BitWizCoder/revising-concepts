5595e7add69d75da916e54d7d4f99b22dfd5c123
BitWizCoder <mhmdnoman01@gmail.com>
2025-06-23 06:22:59 -0700

    'structured'

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,7 @@
-import { useReducer } from "react";
-import AddTask from "./AddTask";
-import TaskList from "./TaskList";
-import { initialTasks } from "./InitialTasks";
-import tasksReducer from "./tasksReducer";
+import TaskApp from "./taskApp/TaskApp";
 
-function App() {
-  const [tasks, dispatch] = useReducer(tasksReducer, initialTasks);
-
-  function handleAddTask(text) {
-    dispatch({
-      type: "added",
-      id: nextId++,
-      text: text,
-    });
-  }
-
-  function handleChangeTask(task) {
-    dispatch({
-      type: "changed",
-      task: task,
-    });
-  }
-
-  function handleDeleteTask(taskId) {
-    dispatch({
-      type: "deleted",
-      id: taskId,
-    });
-  }
-
-  return (
-    <>
-      <h1>Prague itinerary</h1>
-      <AddTask onAddTask={handleAddTask} />
-      <TaskList
-        tasks={tasks}
-        onChangeTask={handleChangeTask}
-        onDeleteTask={handleDeleteTask}
-      />
-    </>
-  );
+export default function App() {
+    return <h1>
+        <TaskApp />
+    </h1>
 }
-
-export default App;
-
-let nextId = 3;
---
+++ src/taskApp/TaskApp.jsx
@@ -0,0 +1,48 @@
+import { useReducer } from "react";
+import AddTask from "./AddTask";
+import TaskList from "./TaskList";
+import { initialTasks } from "./InitialTasks";
+import tasksReducer from "./tasksReducer";
+import './index.css'
+
+function TaskApp() {
+  const [tasks, dispatch] = useReducer(tasksReducer, initialTasks);
+
+  function handleAddTask(text) {
+    dispatch({
+      type: "added",
+      id: nextId++,
+      text: text,
+    });
+  }
+
+  function handleChangeTask(task) {
+    dispatch({
+      type: "changed",
+      task: task,
+    });
+  }
+
+  function handleDeleteTask(taskId) {
+    dispatch({
+      type: "deleted",
+      id: taskId,
+    });
+  }
+
+  return (
+    <>
+      <h1>Prague itinerary</h1>
+      <AddTask onAddTask={handleAddTask} />
+      <TaskList
+        tasks={tasks}
+        onChangeTask={handleChangeTask}
+        onDeleteTask={handleDeleteTask}
+      />
+    </>
+  );
+}
+
+export default TaskApp;
+
+let nextId = 3;
